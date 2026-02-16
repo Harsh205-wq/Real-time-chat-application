e1044b576ff9afb7652902c8ee6b0ed90d018490
Harsh <harshupadhayay23118711xyz@gmail.com>
2026-02-16 10:51:09 +0530

    first commit

---
+++ backend/src/server.js
@@ -0,0 +1,21 @@
+import express from "express"
+import dotenv from "dotenv"
+import authRoutes from "./routes/auth.route.js"
+import messageRoutes from "./routes/message.route.js"
+
+dotenv.config({
+  path:"./.env"  // config dotenv before using process.env.Variable
+})
+
+const app=express();
+
+const PORT=process.env.PORT || 5000
+
+app.use("/api/auth",authRoutes)
+app.use("/api/auth",messageRoutes)
+
+
+
+ app.listen(PORT,()=>{
+    console.log(`Server is running on ${PORT}`)
+  })
